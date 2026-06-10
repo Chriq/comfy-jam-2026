5a6ded905fb980e877b5b60e65599ea3d65fc87c
Chriq <christiancook52@gmail.com>
2026-06-10 02:38:54 -0400

    time of day logic

--- src/Data/Characters/Character.cs
+++ src/Data/Characters/Character.cs
@@ -3,14 +3,14 @@ using System;
 
 [GlobalClass]
 public partial class Character : Resource {
-	[Export] public string displayName;
-	[Export] public Texture2D texture;
-	[Export] public Godot.Collections.Array<Drink> drinkList;
-	public int reputation = 1;
-	
-	public void increaseReputation() {
-		if(reputation < drinkList.Count-1) {
-			reputation++;
-		}
-	}
+    [Export] public string displayName;
+    [Export] public Texture2D texture;
+    [Export] public Godot.Collections.Array<Drink> drinkList;
+    public int reputation = 0;
+
+    public void increaseReputation() {
+        if (reputation < drinkList.Count - 1) {
+            reputation++;
+        }
+    }
 }
