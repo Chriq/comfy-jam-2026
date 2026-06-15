deaa96191a650dd60687eca768c8c3c679afd476
Chriq <39427604+Chriq@users.noreply.github.com>
2026-06-15 19:03:08 -0400

    fix scaling, add font

--- src/Scenes/MainMenu/MainMenu.cs
+++ src/Scenes/MainMenu/MainMenu.cs
@@ -36,11 +36,13 @@ public partial class MainMenu : Control {
 
     private async Task DoIntroCutscene() {
         cutsceneContainer.MouseFilter = MouseFilterEnum.Stop;
+        creditsContainer.Hide();
+        howContainer.Hide();
         FadeController fade = new();
         AddChild(fade);
 
         await fade.FadeIn(cutsceneContainer, 2f);
-        await dm.DisplayText("Hey, your first shift at the bar starts tomorrow morning! Here's a recipe book you can use for reference. The last guy wrote it though, so it may not be super reliable...\n\nOh well, you'll figure it out. Just update the book as you go along. I hope you're better than the last guy!");
+        await dm.DisplayText("Hey, your first shift at the bar starts tomorrow morning!\n\nHere's a recipe book you can use for reference. The last guy wrote it though, so it may not be super reliable...\n\nOh well, you'll figure it out. Just update the book as you go along. I hope you're better than the last guy!\n\n\n\n\n\n[i](Click anywhere to continue)[/i]");
         await ToSignal(dm, "Finished");
 
         GetTree().ChangeSceneToPacked(toScene);
