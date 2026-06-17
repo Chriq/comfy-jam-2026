7ef43c4d0d9bd405c337e6b244110d3f0adb37af
Calvin <66183987+theLzyDog@users.noreply.github.com>
2026-06-17 16:38:45 -0700

    Improved DrinkFeedback

--- src/Systems/DrinkBuilder/DrinkBuilder.cs
+++ src/Systems/DrinkBuilder/DrinkBuilder.cs
@@ -45,11 +45,14 @@ public partial class DrinkBuilder : Node {
 		currentDrink.Clear();
 	}
 
-	// TODO: Make feedback more specific
-	// only return 1 critique at a time: priority = liquor, juice/mixer, garnish
 	public string GetDrinkFeedback(Dictionary<Ingredient, int> drinkOrdered, Dictionary<Ingredient, int> drinkServed) {
 		int critique = 3; //This holds the ingredientType that is currently the highest priority problem. 3 means no issue.
+		int[] levels = [0,0,0]; //Used to see if the drink needs more or less of each ingredientType
+		foreach( System.Collections.Generic.KeyValuePair<Ingredient, int> kvp in drinkOrdered) {
+			levels[(int)kvp.Key.ingredientType] -= kvp.Value;
+		}
 		foreach( System.Collections.Generic.KeyValuePair<Ingredient, int> kvp in drinkServed){
+			levels[(int)kvp.Key.ingredientType] += kvp.Value;
 			if(!drinkOrdered.TryGetValue(kvp.Key, out int value) || value != kvp.Value) {
 				if ((int)kvp.Key.ingredientType < critique) {
 					critique = (int)kvp.Key.ingredientType;
@@ -58,14 +61,33 @@ public partial class DrinkBuilder : Node {
 		}
 		switch (critique) {
 			case (int)IngredientType.LIQUOR:
-				return "Adjust Liquor";
+				if (levels[(int)IngredientType.LIQUOR] > 0) {
+					return "Less Liquor";
+				}else if(levels[(int)IngredientType.LIQUOR] < 0) {
+					return "More Liquor";
+				} else {
+					return "Wrong type of liquor";
+				}
 			case (int)IngredientType.JUICE:
-				return "Adjust Juice";
+				if (levels[(int)IngredientType.JUICE] > 0) {
+					return "Less Juice";
+				}else if(levels[(int)IngredientType.JUICE] < 0) {
+					return "More Juice";
+				} else {
+					return "Wrong type of juice";
+				}
 			case (int)IngredientType.GARNISH:
-				return "Adjust Garnish";
+				if (levels[(int)IngredientType.GARNISH] > 0) {
+					return "Less Garnish";
+				}else if(levels[(int)IngredientType.GARNISH] < 0) {
+					return "More Garnish";
+				} else {
+					return "Wrong type of garnish";
+				}
 			case 3:
 				return "No Issues";
 		}
+		
 		return "ERROR";
 	}
 
