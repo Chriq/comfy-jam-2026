d148ffd56f6def2366e5793b4edb23d86f74ef46
Calvin <66183987+theLzyDog@users.noreply.github.com>
2026-06-19 15:10:15 -0700

    Recipe Randomizer

--- src/Prefabs/RecipeBook/RecipePage.cs
+++ src/Prefabs/RecipeBook/RecipePage.cs
@@ -4,66 +4,73 @@ using System.Collections.Generic;
 using System.Linq;
 
 public partial class RecipePage : Control {
-    [Export] RichTextLabel header;
-    [Export] TextureRect img;
-    [Export] RichTextLabel description;
-    [Export] Control recipe;
+	[Export] RichTextLabel header;
+	[Export] TextureRect img;
+	[Export] RichTextLabel description;
+	[Export] Control recipe;
+	[Export] float mistakeChance;
+	[Export] PackedScene listItemPrefab;
 
-    [Export] PackedScene listItemPrefab;
+	public void InitPage(Drink drink) {
+		if (drink != null) {
+			header.Text = drink.displayName;
+			description.Text = $"[i] {drink.description} [/i]";
+			img.Texture = drink.texture;
+			// recipe.Text = GetRecipeString(drink);
+			InitRecipeItems(drink);
+		} else {
+			header.Text = "";
+			img.Texture = null;
+			description.Text = "";
+			foreach (Node n in recipe.GetChildren()) recipe.RemoveChild(n);
+		}
+	}
 
-    public void InitPage(Drink drink) {
-        if (drink != null) {
-            header.Text = drink.displayName;
-            description.Text = $"[i] {drink.description} [/i]";
-            img.Texture = drink.texture;
-            // recipe.Text = GetRecipeString(drink);
-            InitRecipeItems(drink);
-        } else {
-            header.Text = "";
-            img.Texture = null;
-            description.Text = "";
-            foreach (Node n in recipe.GetChildren()) recipe.RemoveChild(n);
-        }
-    }
+	private string GetRecipeString(Drink drink) {
+		string recipeStringBuilder = "Recipe:\n";
 
-    private string GetRecipeString(Drink drink) {
-        string recipeStringBuilder = "Recipe:\n";
+		foreach (Ingredient i in drink.recipe.Keys) {
+			recipeStringBuilder += $"- {i.displayName} {drink.recipe[i]} {i.units}\n";
+		}
 
-        foreach (Ingredient i in drink.recipe.Keys) {
-            recipeStringBuilder += $"- {i.displayName} {drink.recipe[i]} {i.units}\n";
-        }
+		return recipeStringBuilder;
+	}
 
-        return recipeStringBuilder;
-    }
+	private void InitRecipeItems(Drink drink) {
+		foreach(Node n in recipe.GetChildren()) {
+			recipe.RemoveChild(n);
+		}
+		
+		List<Ingredient> sortedIngredients = drink.recipe.Keys.ToList();
+		sortedIngredients.Sort((a,b) => {
+			return a.ingredientType - b.ingredientType;
+		});
 
-    private void InitRecipeItems(Drink drink) {
-        foreach(Node n in recipe.GetChildren()) {
-            recipe.RemoveChild(n);
-        }
-        
-        List<Ingredient> sortedIngredients = drink.recipe.Keys.ToList();
-        sortedIngredients.Sort((a,b) => {
-            return a.ingredientType - b.ingredientType;
-        });
+		foreach (Ingredient i in sortedIngredients) {
+			Node item = listItemPrefab.Instantiate();
+			SpinBox s = item.GetChild<SpinBox>(0);
+			s.Suffix = i.units;
+			int val = 0;
+			float randValue = GD.Randf();
+			if(randValue < mistakeChance){
+				val = Mathf.Clamp(drink.recipe[i] + GD.RandRange(-1, 1), 1, (int) s.MaxValue);
+				GD.Print(val);
+			} else {
+				val = drink.recipe[i];
+			}
+			// int val = drink.recipe[i];
+			s.Value = val;
 
-        foreach (Ingredient i in sortedIngredients) {
-            Node item = listItemPrefab.Instantiate();
-            SpinBox s = item.GetChild<SpinBox>(0);
-            s.Suffix = i.units;
-            int val = Mathf.Clamp(drink.recipe[i] + GD.RandRange(-1, 1), 1, (int) s.MaxValue);
-            // int val = drink.recipe[i];
-            s.Value = val;
+			RichTextLabel l = item.GetChild<RichTextLabel>(1);
+			l.Text = i.displayName;
 
-            RichTextLabel l = item.GetChild<RichTextLabel>(1);
-            l.Text = i.displayName;
+			item.RemoveChild(s);
+			recipe.AddChild(s);
 
-            item.RemoveChild(s);
-            recipe.AddChild(s);
+			item.RemoveChild(l);
+			recipe.AddChild(l);
 
-            item.RemoveChild(l);
-            recipe.AddChild(l);
-
-            item.QueueFree();
-        }
-    }
+			item.QueueFree();
+		}
+	}
 }
